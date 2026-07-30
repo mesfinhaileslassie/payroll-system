b9cacf6bf6431826de0b8772b046728087860f7c
Mesfin <mesfinhaileslassie17@gmail.com>
2026-07-30 12:18:51 +0300

    load employees fixed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:5062/api';
+const API_BASE_URL ='http://127.0.0.1:5062/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
